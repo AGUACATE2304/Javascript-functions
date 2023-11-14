e89293f6242a57375abb7c1529a9897cbeca2203
AGUACATE2304 <alan.castillo@mataro.epiaedu.cat>
2023-11-14 20:19:55 +0100

    Create .github + completed exercise_01

--- exercise_01/getDistanceFromThreshold.js
+++ exercise_01/getDistanceFromThreshold.js
@@ -4,3 +4,23 @@
  * input: number (threshold)
  * output: number
  */
+
+/**
+ * Check if a number exceeds or is left to reach one value
+ * @param {number} value
+ * @param {number} threshold
+ * @returns number
+ */
+const getDistanceFromThreshold = function (value, threshold) {
+  if (value > threshold) {
+    value = value - threshold;
+    return value;
+  }
+
+  if (threshold > value) {
+    threshold = threshold - value;
+    return threshold;
+  }
+};
+
+export default getDistanceFromThreshold;
