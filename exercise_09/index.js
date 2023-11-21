7a033051ca2c956da51b86d08e0f2374222dee42
AGUACATE2304 <alan.castillo@mataro.epiaedu.cat>
2023-11-21 20:22:51 +0100

    Add test on comment on console.log()

--- exercise_09/index.js
+++ exercise_09/index.js
@@ -1,7 +1,7 @@
 import getDistanceMessageFromSumTo100 from "./getDistanceMessageFromSumTo100.js";
 
 const under100Value = 0;
-console.log(getDistanceMessageFromSumTo100(under100Value));
+console.log(getDistanceMessageFromSumTo100(under100Value)); // Sum with value 70 is left in 30 from number 100
 
 const upper100Value = 100;
-console.log(getDistanceMessageFromSumTo100(upper100Value));
+console.log(getDistanceMessageFromSumTo100(upper100Value)); // Sum with value 155 exceeds in 55 from number 100
